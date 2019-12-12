e1c7fbe1bfb58d4b17f2bebe83129e2d6a0ca131
adminRAjeev <rajeev.glocify@gmail.com>
2019-12-12 16:51:56 +0530

    newTest

--- test.php
+++ test.php
@@ -3,4 +3,8 @@ echo "To start working locally on an existing remote repository, clone it with t
 echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
 echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
 echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
+
+Switch to the master branch
+
+You are always in a branch when working with Git. The main branch is the master branch, but you can use the same command to switch to a different branch by changing master to the branch name.
 ?>

84c8d7b791cd97dc9435c42d7835e2d8d2ea4a8c
adminRAjeev <rajeev.glocify@gmail.com>
2019-12-12 16:36:34 +0530

    newChanges

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
-echo "hjhjjkk";
+echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
+echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
+echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
 echo "To start working locally on an existing remote repository, clone it with the command git clone <repository path>. By cloning a repository, you’ll download a copy of its files to your local computer, automatically preserving the Git connection with the remote repository.";
 ?>
